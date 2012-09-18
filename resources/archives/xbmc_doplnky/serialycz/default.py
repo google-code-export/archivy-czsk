c552900ba06d590b4ba4b8b997eaa52545987507
mx3Lmail@gmail.com <mx3Lmail@gmail.com@c3dbcfae-13be-b778-7c72-20f6e90116ec>
2012-09-18 05:00:23 +0000

    serialycz- opravena mozna chyba pri nacitani informacii o serialoch

--- resources/archives/xbmc_doplnky/serialycz/default.py
+++ resources/archives/xbmc_doplnky/serialycz/default.py
@@ -19,7 +19,7 @@
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
 # */
-import urllib2,re,os,md5,sys
+import urllib2, re, os, md5, sys
 try:
     import Plugins.Extensions.archivCZSK.resources.archives.xbmc_doplnky.tools.util as util
     import Plugins.Extensions.archivCZSK.resources.archives.xbmc_doplnky.tools.resolver as resolver
@@ -33,17 +33,17 @@ try:
 except ImportError:
     print 'unit test'
 
-__scriptid__   = 'plugin.video.serialycz.cz'
+__scriptid__ = 'plugin.video.serialycz.cz'
 __scriptname__ = 'serialycz.cz'
 
-BASE_URL='http://www.serialycz.cz/'
+BASE_URL = 'http://www.serialycz.cz/'
 
 def getContent(url, **kwargs):
-    name=kwargs['name'] 
+    name = kwargs['name'] 
     p = {} 
     if url is not None:
         p = url   
-    if p=={}:
+    if p == {}:
          #xbmc.executebuiltin('RunPlugin(plugin://script.usage.tracker/?do=reg&cond=31000&id=%s)' % __scriptid__)
             list_series()
     if 'newest' in p.keys():
@@ -51,111 +51,115 @@ def getContent(url, **kwargs):
     if 'serie' in p.keys():
                 list_episodes(p['serie'])
     if 'play' in p.keys():
-                play(p['play'],name)
+                play(p['play'], name)
 
 
-def _get_meta(name,link):
+def _get_meta(name, link):
         # load meta from disk or download it (slow for each serie, thatwhy we cache it)
         local = xbmc.translatePath(__addon__.getAddonInfo('profile'))
         if not os.path.exists(local):
                 os.makedirs(local)
         m = md5.new()
         m.update(name)
-        image = os.path.join(local,m.hexdigest()+'_img.url')
-        plot = os.path.join(local,m.hexdigest()+'_plot.txt')
+        image = os.path.join(local, m.hexdigest() + '_img.url')
+        plot = os.path.join(local, m.hexdigest() + '_plot.txt')
         if not os.path.exists(image):
                 data = util.request(link)
-                data = util.substr(data,'<div id=\"archive-posts\"','</div>')
+                data = util.substr(data, '<div id=\"archive-posts\"', '</div>')
                 m = re.search('<a(.+?)href=\"(?P<url>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
                 if not m == None:
                         data = util.request(m.group('url'))
-                        _get_image(data,image)
-                        _get_plot(data,plot)
-        return _load(image).strip(),_load(plot)
+                        _get_image(data, image)
+                        _get_plot(data, plot)
+        return _load(image).strip(), _load(plot)
     
 
-def _get_meta2(name,link):
+def _get_meta2(name, link):
         # load meta from disk or download it (slow for each tale, thatwhy we cache it)
                    
                 data = util.request(link)
-                data = util.substr(data,'<div id=\"archive-posts\"','</div>')
+                data = util.substr(data, '<div id=\"archive-posts\"', '</div>')
                 m = re.search('<a(.+?)href=\"(?P<url>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
                 if not m == None:
                     data = util.request(m.group('url'))
-                    image=_get_image(data)
-                    plot=_get_plot(data)
-                    return image,plot      
+                    image = _get_image(data)
+                    plot = _get_plot(data)
+                    return image, plot      
         
 
-def _save(data,local):
+def _save(data, local):
         print 'Saving file %s' % local
-        f = open(local,'w')
+        f = open(local, 'w')
         f.write(data)
         f.close()
 
 def _load(file):
         if not os.path.exists(file):
                 return ''
-        f = open(file,'r')
+        f = open(file, 'r')
         data = f.read()
         f.close()
         return data
 
 def _get_plot(data):
-                data = util.substr(data,'<div class=\"entry-content\"','</p>')
+                data = util.substr(data, '<div class=\"entry-content\"', '</p>')
                 m = re.search('<(strong|b)>(?P<plot>(.+?))<', data, re.IGNORECASE | re.DOTALL)
                 if not m == None:
                        # _save(util.decode_html(m.group('plot')).encode('utf-8'),local)
                        return util.decode_html(m.group('plot')).encode('utf-8')
 def _get_image(data):
-                data = util.substr(data,'<div class=\"entry-photo\"','</div>')
+                data = util.substr(data, '<div class=\"entry-photo\"', '</div>')
                 m = re.search('<img(.+?)src=\"(?P<img>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
                 if not m == None:
                     return m.group('img')
                        # _save(m.group('img'),local)
         
 def list_series():
-        data = util.substr(util.request(BASE_URL),'<div id=\"primary\"','</div>')
-        pattern='<a href=\"(?P<link>[^\"]+)[^>]+>(?P<name>[^<]+)</a>'   
-        add_dir('Nejnovější epizody',{'newest':'list'},util.icon('new.png'))
+        data = util.substr(util.request(BASE_URL), '<div id=\"primary\"', '</div>')
+        pattern = '<a href=\"(?P<link>[^\"]+)[^>]+>(?P<name>[^<]+)</a>'   
+        add_dir('Nejnovější epizody', {'newest':'list'}, util.icon('new.png'))
         #util.add_local_dir(__language__(30037),__addon__.getSetting('downloads'),util.icon('download.png'))
-        for m in re.finditer(pattern, util.substr(data,'Seriály</a>','</ul>'), re.IGNORECASE | re.DOTALL):
-                image,plot = _get_meta2(m.group('name'),m.group('link'))
-                add_dir(m.group('name'),{'serie':m.group('link')[len(BASE_URL):]},image,infoLabels={'Plot':plot})
+        for m in re.finditer(pattern, util.substr(data, 'Seriály</a>', '</ul>'), re.IGNORECASE | re.DOTALL):
+            try:
+                image, plot = _get_meta2(m.group('name'), m.group('link'))
+            except:
+                image = None
+                plot = None    
+            add_dir(m.group('name'), {'serie':m.group('link')[len(BASE_URL):]}, image, infoLabels={'Plot':plot})
        # xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def list_episodes(url): 
-        data = util.request(BASE_URL+url)
-        data = util.substr(data,'<div id=\"archive-posts\"','</div>')
+        data = util.request(BASE_URL + url)
+        data = util.substr(data, '<div id=\"archive-posts\"', '</div>')
         m = re.search('<a(.+?)href=\"(?P<url>[^\"]+)', data, re.IGNORECASE | re.DOTALL)
         if not m == None:
                 data = util.request(m.group('url'))
-                for m in re.finditer('<a href=\"(?P<link>[^\"]+)(.+?)(<strong>|<b>)(?P<name>[^<]+)', util.substr(data,'<div class=\"entry-content','</div>'), re.IGNORECASE | re.DOTALL):
-                        add_video2(m.group('name'),m.group('link'),'')
+                for m in re.finditer('<a href=\"(?P<link>[^\"]+)(.+?)(<strong>|<b>)(?P<name>[^<]+)', util.substr(data, '<div class=\"entry-content', '</div>'), re.IGNORECASE | re.DOTALL):
+                        add_video2(m.group('name'), m.group('link'), '')
              #   xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def newest_episodes():
-        data = util.substr(util.request(BASE_URL+'category/new-episode/'),'<div id=\"archive-posts\"','</ul>')
-        pattern='<img(.+?)src=\"(?P<img>[^\"]+)(.+?)<a href=\"(?P<link>[^\"]+)[^>]+>(?P<name>[^<]+)</a>'        
+        data = util.substr(util.request(BASE_URL + 'category/new-episode/'), '<div id=\"archive-posts\"', '</ul>')
+        pattern = '<img(.+?)src=\"(?P<img>[^\"]+)(.+?)<a href=\"(?P<link>[^\"]+)[^>]+>(?P<name>[^<]+)</a>'        
         for m in re.finditer(pattern, data, re.IGNORECASE | re.DOTALL):
-                add_video2(m.group('name'),m.group('link'),m.group('img'))
+                add_video2(m.group('name'), m.group('link'), m.group('img'))
       #  xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
-def add_video2(name,url,image=''):
-       return add_video(name,{'play':url},image)#menuItems={xbmc.getLocalizedString(33003):{'name':name+'.mp4','download':url}})
+def add_video2(name, url, image=''):
+       return add_video(name, {'play':url}, image)#menuItems={xbmc.getLocalizedString(33003):{'name':name+'.mp4','download':url}})
 
 def resolve(url):
         if not url.startswith('http'):
-                url = BASE_URL+url
-        data = util.substr(util.request(url),'<div id=\"content\"','#content')
-        resolved = resolver.findstreams(data,['<embed( )src=\"(?P<url>[^\"]+)','<object(.+?)data=\"(?P<url>[^\"]+)','<iframe(.+?)src=[\"\'](?P<url>.+?)[\'\"]'])
+                url = BASE_URL + url
+        data = util.substr(util.request(url), '<div id=\"content\"', '#content')
+        resolved = resolver.findstreams(data, ['<embed( )src=\"(?P<url>[^\"]+)', '<object(.+?)data=\"(?P<url>[^\"]+)', '<iframe(.+?)src=[\"\'](?P<url>.+?)[\'\"]'])
         if resolved == None:
                 #xbmcgui.Dialog().ok(__scriptname__,__language__(30001))
                 return
         if not resolved == {}:
                 return resolved
 
-def play(url,name):
+def play(url, name):
         streams = resolve(url)
         if streams:
             if len(streams) > 0:
