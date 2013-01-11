168d31adee6f2f4d5c1cd163866914c94877b5d0
mx3Lmail@gmail.com <mx3Lmail@gmail.com@c3dbcfae-13be-b778-7c72-20f6e90116ec>
2013-01-11 22:07:04 +0000

    efektivnejsi logging system - nevyhodnocuju sa zbytocne log spravy, pri vypnutom loggingu -> citelne zrychlenie celeho pluginu

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-import os,gettext,sys
- 
+import os, gettext, sys
+#from logging import StreamHandler
  
 PluginLanguageDomain = "archivCZSK"
 PluginLanguagePath = "Extensions/archivCZSK/locale"
@@ -17,9 +16,55 @@ def localeInit():
 def _(txt):
     t = gettext.dgettext(PluginLanguageDomain, txt)
     if t == txt:
-        print "[%s] fallback to default translation for %s" %(PluginLanguageDomain, txt)
+        print "[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)
         t = gettext.gettext(txt)
     return t
- 
+    
 localeInit()
 language.addCallback(localeInit)
+
+class log(object):
+    ERROR = 0
+    INFO = 1
+    DEBUG = 2
+    mode = INFO
+    
+    @staticmethod
+    def changeMode(mode):
+        log.mode = mode
+    
+    @staticmethod
+    def debug(text, *args):
+        if log.mode == log.DEBUG:
+            if len(args) == 1 and isinstance(args[0],tuple):
+                text = text % args[0]
+            else:
+                text = text % (args)
+            print "[ArchivCZSK] DEBUG:", text.encode('utf-8')
+    
+    @staticmethod    
+    def info(text, *args):
+        if log.mode > log.INFO:
+            if len(args) == 1 and isinstance(args[0],tuple):
+                text = text % args[0]
+            else:
+                text = text % (args)
+            print "[ArchivCZSK] INFO:", text.encode('utf-8') 
+    
+    @staticmethod  
+    def error(text, *args):
+        if log.mode == log.ERROR:
+            if len(args) == 1 and isinstance(args[0],tuple):
+                text = text % args[0]
+            else:
+                text = text % (args)
+            print "[ArchivCZSK] ERROR:", text.encode('utf-8') 
+  
+
+# set logger
+#default_formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
+#console_handler = StreamHandler()
+#console_handler.setFormatter(default_formatter)
+#root = logging.getLogger(__name__)
+#root.addHandler(console_handler)
+#root.setLevel(logging.DEBUG)
